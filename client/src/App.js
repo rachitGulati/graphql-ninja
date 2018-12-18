9f75970b629c72f3ac5cd3bde19229196ddc88e1
rachitGulati <rachitgulati26@gmail.com>
2018-12-18 09:52:24 +0530

    Setup the right graphql server

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import AddBook from './components/AddBook';
 
 // Apollo Client SetUp
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql'
+  uri: '/graphql'
 });
 
 class App extends Component {
