ffd45370f52d3b05335d886145a163a16c796ce8
rachitGulati <rachitgulati26@gmail.com>
2018-12-18 15:30:04 +0530

    Added the basic netlify check to build the application

---
+++ server/index-local.js
@@ -0,0 +1,25 @@
+const schema = require('./schema/schema');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = 4000;
+const app = express();
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+
+app.use(cors());
+mongoose.connect(`mongodb://${DB_USER}:${DB_PASS}@ds131784.mlab.com:31784/graphql-ninja`);
+mongoose.connection.once('open', ()=>{
+    console.log('connected to databse');
+})
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(PORT, ()=>{
+    console.log('Server is listing at ' + PORT);
+})
--- server/index.js
+++ server/index.js
@@ -2,24 +2,38 @@ const schema = require('./schema/schema');
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const mongoose = require('mongoose');
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 
 app.use(cors());
-mongoose.connect(`mongodb://${DB_USER}:${DB_PASS}@ds131784.mlab.com:31784/graphql-ninja`);
+mongoose.connect(`mongodb://rachit:rachit1234@ds131784.mlab.com:31784/graphql-ninja`);
 mongoose.connection.once('open', ()=>{
     console.log('connected to databse');
 })
+
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
 
-app.listen(PORT, ()=>{
-    console.log('Server is listing at ' + PORT);
-})
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
